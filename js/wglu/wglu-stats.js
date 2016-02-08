a4e8c7763ce0a3a55d0e96e686beb5634fc015f8
Brandon Jones <bajones@chromium.org>
2016-02-07 21:51:43 -0800

    Added ortho display to stats

--- js/wglu/wglu-stats.js
+++ js/wglu/wglu-stats.js
@@ -33,7 +33,7 @@ var WGLUStats = (function() {
 
     "void main() {",
     "  vColor = vec4(color, 1.0);",
-    "  gl_Position = projectionMat * modelViewMat * vec4( position.xy, position.z - 2.0, 1.0 );",
+    "  gl_Position = projectionMat * modelViewMat * vec4( position, 1.0 );",
     "}",
   ].join("\n");
 
@@ -57,6 +57,14 @@ var WGLUStats = (function() {
     return (Math.min(value, maxFPS) * (0.7 / maxFPS)) - 0.45;
   }
 
+  function fpsToRGB(value) {
+    return {
+      r: Math.max(0.0, Math.min(1.0, 1.0 - (value/60))),
+      g: Math.max(0.0, Math.min(1.0, ((value-15)/(maxFPS-15)))),
+      b: Math.max(0.0, Math.min(1.0, ((value-15)/(maxFPS-15))))
+    };
+  }
+
   var now = ( performance && performance.now ) ? performance.now.bind( performance ) : Date.now;
 
   var Stats = function(gl) {
@@ -66,6 +74,13 @@ var WGLUStats = (function() {
     this.prevTime = this.startTime;
     this.frames = 0;
     this.fps = 0;
+
+    this.scale = vec3.fromValues(1, 1, 1);
+    this.position = vec3.fromValues(0, 0, 0);
+    this.orientation = quat.fromValues(0, 0, 0, 1);
+
+    this.orthoMatrix = mat4.create();
+    this.matrix = mat4.create();
 
     this.lastSegment = 0;
 
@@ -109,16 +124,16 @@ var WGLUStats = (function() {
     };
 
     // Panel Background
-    addBGSquare(-0.5, -0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.25);
+    addBGSquare(-0.5, -0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.125);
 
     // FPS Background
-    addBGSquare(-0.45, -0.45, 0.45, 0.25, 0.01, 0.0, 0.0, 0.125);
+    addBGSquare(-0.45, -0.45, 0.45, 0.25, 0.01, 0.0, 0.0, 0.25);
 
     // 30 FPS line
-    addBGSquare(-0.45, fpsToY(30), 0.45, fpsToY(30.5), 0.015, 0.5, 0.0, 0.5);
+    addBGSquare(-0.45, fpsToY(30), 0.45, fpsToY(32), 0.015, 0.5, 0.0, 0.5);
 
     // 60 FPS line
-    addBGSquare(-0.45, fpsToY(60), 0.45, fpsToY(60.5), 0.015, 0.2, 0.0, 0.75);
+    addBGSquare(-0.45, fpsToY(60), 0.45, fpsToY(62), 0.015, 0.2, 0.0, 0.75);
 
     this.fpsVertBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this.fpsVertBuffer);
@@ -151,38 +166,36 @@ var WGLUStats = (function() {
   };
 
   Stats.prototype.updateGraph = function(value) {
-    var r = 1.0 - (value/maxFPS);
-    var g = (value/maxFPS);
-    var b = (value/maxFPS);
+    var color = fpsToRGB(value);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, this.fpsVertBuffer);
 
     var updateVerts = [
-      segmentToX(this.lastSegment), fpsToY(value), 0.02, r, g, b,
-      segmentToX(this.lastSegment+1), fpsToY(value), 0.02, r, g, b,
-      segmentToX(this.lastSegment), fpsToY(0), 0.02, r, g, b,
-      segmentToX(this.lastSegment+1), fpsToY(0), 0.02, r, g, b,
+      segmentToX(this.lastSegment), fpsToY(value), 0.02, color.r, color.g, color.b,
+      segmentToX(this.lastSegment+1), fpsToY(value), 0.02, color.r, color.g, color.b,
+      segmentToX(this.lastSegment), fpsToY(0), 0.02, color.r, color.g, color.b,
+      segmentToX(this.lastSegment+1), fpsToY(0), 0.02, color.r, color.g, color.b,
     ];
 
-    r = 0.2;
-    g = 1.0;
-    b = 0.2;
+    color.r = 0.2;
+    color.g = 1.0;
+    color.b = 0.2;
 
     if (this.lastSegment == segments - 1) {
       gl.bufferSubData(gl.ARRAY_BUFFER, this.lastSegment * 24 * 4, new Float32Array(updateVerts));
       updateVerts = [
-        segmentToX(0), fpsToY(maxFPS), 0.02, r, g, b,
-        segmentToX(.25), fpsToY(maxFPS), 0.02, r, g, b,
-        segmentToX(0), fpsToY(0), 0.02, r, g, b,
-        segmentToX(.25), fpsToY(0), 0.02, r, g, b
+        segmentToX(0), fpsToY(maxFPS), 0.02, color.r, color.g, color.b,
+        segmentToX(.25), fpsToY(maxFPS), 0.02, color.r, color.g, color.b,
+        segmentToX(0), fpsToY(0), 0.02, color.r, color.g, color.b,
+        segmentToX(.25), fpsToY(0), 0.02, color.r, color.g, color.b
       ];
       gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(updateVerts));
     } else {
       updateVerts.push(
-        segmentToX(this.lastSegment+1), fpsToY(maxFPS), 0.02, r, g, b,
-        segmentToX(this.lastSegment+1.25), fpsToY(maxFPS), 0.02, r, g, b,
-        segmentToX(this.lastSegment+1), fpsToY(0), 0.02, r, g, b,
-        segmentToX(this.lastSegment+1.25), fpsToY(0), 0.02, r, g, b
+        segmentToX(this.lastSegment+1), fpsToY(maxFPS), 0.02, color.r, color.g, color.b,
+        segmentToX(this.lastSegment+1.25), fpsToY(maxFPS), 0.02, color.r, color.g, color.b,
+        segmentToX(this.lastSegment+1), fpsToY(0), 0.02, color.r, color.g, color.b,
+        segmentToX(this.lastSegment+1.25), fpsToY(0), 0.02, color.r, color.g, color.b
       );
       gl.bufferSubData(gl.ARRAY_BUFFER, this.lastSegment * 24 * 4, new Float32Array(updateVerts));
     }
@@ -211,6 +224,19 @@ var WGLUStats = (function() {
     gl.drawElements(gl.TRIANGLES, this.fpsIndexCount, gl.UNSIGNED_SHORT, 0);
 
     // TODO: Render FPS text
+  }
+
+  Stats.prototype.renderOrtho = function(x, y, width, height) {
+    var canvas = this.gl.canvas;
+
+    mat4.ortho(this.orthoMatrix, 0, canvas.width, 0, canvas.height, 0.1, 1024);
+
+    mat4.identity(this.matrix);
+    mat4.translate(this.matrix, this.matrix, [x, canvas.height - height - y, -1]);
+    mat4.scale(this.matrix, this.matrix, [width, height, 1]);
+    mat4.translate(this.matrix, this.matrix, [0.5, 0.5, 0]);
+
+    this.render(this.orthoMatrix, this.matrix);
   }
 
   return Stats;
