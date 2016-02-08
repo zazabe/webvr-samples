4022ad9a37bf7b12bce095ef520b1938ec2938c2
Brandon Jones <bajones@chromium.org>
2016-02-08 11:42:17 -0800

    Added a seven segment LCD display to stats
    Now prints out the FPS

--- js/third-party/wglu/wglu-stats.js
+++ js/third-party/wglu/wglu-stats.js
@@ -24,6 +24,179 @@ var WGLUStats = (function() {
 
   "use strict";
 
+  var sevenSegmentVS = [
+    "uniform mat4 projectionMat;",
+    "uniform mat4 modelViewMat;",
+    "attribute vec2 position;",
+
+    "void main() {",
+    "  gl_Position = projectionMat * modelViewMat * vec4( position, 0.0, 1.0 );",
+    "}",
+  ].join("\n");
+
+  var sevenSegmentFS = [
+    "precision mediump float;",
+    "uniform vec4 color;",
+
+    "void main() {",
+    "  gl_FragColor = color;",
+    "}",
+  ].join("\n");
+
+  var SevenSegmentText = function (gl) {
+    this.gl = gl;
+
+    this.program = new WGLUProgram(gl);
+    this.program.attachShaderSource(sevenSegmentVS, gl.VERTEX_SHADER);
+    this.program.attachShaderSource(sevenSegmentFS, gl.FRAGMENT_SHADER);
+    this.program.bindAttribLocation({
+      position: 0,
+      color: 1
+    });
+    this.program.link();
+
+    var verts = [];
+    var segmentIndices = {};
+    var indices = [];
+
+    var width = 0.5;
+    var thickness = 0.25;
+    this.kerning = 2.0;
+
+    this.matrix = mat4.create();
+
+    function defineSegment(id, left, top, right, bottom) {
+      var idx = verts.length / 2;
+      verts.push(
+          left, top,
+          right, top,
+          right, bottom,
+          left, bottom);
+
+      segmentIndices[id] = [
+          idx, idx+1, idx+2,
+          idx, idx+2, idx+3];
+    }
+
+    var characters = {};
+    this.characters = characters;
+
+    function defineCharacter(c, segments) {
+      var character = {
+        character: c,
+        offset: indices.length * 2,
+        count: 0
+      };
+
+      for (var i = 0; i < segments.length; ++i) {
+        var idx = segments[i];
+        var segment = segmentIndices[idx];
+        character.count += segment.length;
+        indices.push.apply(indices, segment);
+      }
+
+      characters[c] = character;
+    }
+
+    /* Segment layout is as follows:
+
+    |-0-|
+    3   4
+    |-1-|
+    5   6
+    |-2-|
+
+    */
+
+    defineSegment(0, -1, 1, width, 1-thickness);
+    defineSegment(1, -1, thickness*0.5, width, -thickness*0.5);
+    defineSegment(2, -1, -1+thickness, width, -1);
+    defineSegment(3, -1, 1, -1+thickness, -thickness*0.5);
+    defineSegment(4, width-thickness, 1, width, -thickness*0.5);
+    defineSegment(5, -1, thickness*0.5, -1+thickness, -1);
+    defineSegment(6, width-thickness, thickness*0.5, width, -1);
+
+
+    defineCharacter("0", [0, 2, 3, 4, 5, 6]);
+    defineCharacter("1", [4, 6]);
+    defineCharacter("2", [0, 1, 2, 4, 5]);
+    defineCharacter("3", [0, 1, 2, 4, 6]);
+    defineCharacter("4", [1, 3, 4, 6]);
+    defineCharacter("5", [0, 1, 2, 3, 6]);
+    defineCharacter("6", [0, 1, 2, 3, 5, 6]);
+    defineCharacter("7", [0, 4, 6]);
+    defineCharacter("8", [0, 1, 2, 3, 4, 5, 6]);
+    defineCharacter("9", [0, 1, 2, 3, 4, 6]);
+    defineCharacter("A", [0, 1, 3, 4, 5, 6]);
+    defineCharacter("B", [1, 2, 3, 5, 6]);
+    defineCharacter("C", [0, 2, 3, 5]);
+    defineCharacter("D", [1, 2, 4, 5, 6]);
+    defineCharacter("E", [0, 1, 2, 4, 6]);
+    defineCharacter("F", [0, 1, 3, 5]);
+    defineCharacter("P", [0, 1, 3, 4, 5]);
+    defineCharacter("-", [1]);
+    defineCharacter(" ", []);
+    defineCharacter("_", [2]); // Used for undefined characters
+
+
+    this.vertBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.DYNAMIC_DRAW);
+
+    this.indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+  };
+
+  SevenSegmentText.prototype.render = function(projectionMat, modelViewMat, text, r, g, b, a) {
+    var gl = this.gl;
+    var program = this.program;
+
+    if (r == undefined || g == undefined || b == undefined) {
+      r = 0.0;
+      g = 1.0;
+      b = 0.0;
+    }
+
+    if (a == undefined)
+      a = 1.0;
+
+    program.use();
+
+    gl.uniformMatrix4fv(program.uniform.projectionMat, false, projectionMat);
+    gl.uniform4f(program.uniform.color, r, g, b, a);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertBuffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+    gl.enableVertexAttribArray(program.attrib.position);
+    gl.vertexAttribPointer(program.attrib.position, 2, gl.FLOAT, false, 8, 0);
+
+    text = text.toUpperCase();
+
+    var offset = 0;
+
+    for (var i = 0; i < text.length; ++i) {
+      var c;
+      if (text[i] in this.characters) {
+        c = this.characters[text[i]];
+      } else {
+        c = this.characters["_"];
+      }
+
+      if (c.count != 0) {
+        mat4.fromTranslation(this.matrix, [offset, 0, 0]);
+        mat4.multiply(this.matrix, modelViewMat, this.matrix);
+
+        gl.uniformMatrix4fv(program.uniform.modelViewMat, false, this.matrix);
+        gl.drawElements(gl.TRIANGLES, c.count, gl.UNSIGNED_SHORT, c.offset);
+
+      }
+
+      offset += this.kerning;
+    }
+  }
+
   var statsVS = [
     "uniform mat4 projectionMat;",
     "uniform mat4 modelViewMat;",
@@ -70,6 +243,8 @@ var WGLUStats = (function() {
   var Stats = function(gl) {
     this.gl = gl;
 
+    this.sevenSegmentText = new SevenSegmentText(gl);
+
     this.startTime = now();
     this.prevTime = this.startTime;
     this.frames = 0;
@@ -77,6 +252,7 @@ var WGLUStats = (function() {
 
     this.orthoMatrix = mat4.create();
     this.matrix = mat4.create();
+    this.textMatrix = mat4.create();
 
     this.lastSegment = 0;
 
@@ -140,7 +316,7 @@ var WGLUStats = (function() {
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(fpsIndices), gl.STATIC_DRAW);
 
     this.fpsIndexCount = fpsIndices.length;
-  }
+  };
 
   Stats.prototype.begin = function() {
     this.startTime = now();
@@ -219,6 +395,15 @@ var WGLUStats = (function() {
 
     gl.drawElements(gl.TRIANGLES, this.fpsIndexCount, gl.UNSIGNED_SHORT, 0);
 
+    mat4.identity(this.textMatrix);
+    mat4.translate(this.textMatrix, this.textMatrix, [-0.4, 0.4, 0.0]);
+    mat4.scale(this.textMatrix, this.textMatrix, [0.075, 0.075, 1]);
+    mat4.translate(this.textMatrix, this.textMatrix, [0.5, -0.5, 0.02]);
+
+
+    mat4.multiply(this.textMatrix, modelViewMat, this.textMatrix);
+
+    this.sevenSegmentText.render(projectionMat, this.textMatrix, this.fps + " FP5");
     // TODO: Render FPS text
   }
 
